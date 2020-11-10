a70d2a8dfaf2fa9c9aa6dc25978454958a3dc440
lihongyi <354888502@qq.com>
2020-11-10 13:50:29 +0800

    dhz服务器

---
+++ server/app.js
@@ -0,0 +1,39 @@
+/*引入express框架*/
+const express=require('express');
+//引入body-parser中间件
+const bodyParser=require('body-parser');
+
+
+
+/*********** 引入路由器 ***********/
+const userRouter=require('./router/user.js'); //用户路由器
+
+
+/*********** 引入路由器 ***********/
+
+
+
+
+
+/*创建服务器*/
+const app=express();
+app.listen(8080);
+
+
+/*托管静态资源*/
+app.use(express.static('./public'));
+
+
+/*使用body-parser中间件*/
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+
+/*********** 使用路由器并增加前缀 ***********/
+
+app.use('/user',userRouter);//前缀/user
+
+
+
+/*********** 使用路由器并增加前缀 ***********/
---
+++ server/router/user.js
@@ -0,0 +1,20 @@
+/*引入express框架*/
+const express=require('express');
+//引入连接池
+const pool=require('../pool.js');
+//创建用户路由器
+const u=express.Router();
+
+
+/*添加路由*/
+
+
+
+
+
+
+
+
+
+//导出路由
+module.exports=u;
