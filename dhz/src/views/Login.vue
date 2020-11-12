717d6b63aef6dbd9f29c95cf32ba27e7890438ca
liran <liran@123.com>
2020-11-12 10:43:47 +0800

    登录注册第一次提交

---
+++ dhz/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login">
+    <van-field  type="digit" placeholder="请输入您的手机号" left-icon="friends-o"/>
+    <van-field
+      left-icon="goods-collect-o"
+        placeholder="请输入您的密码"
+      >
+        <template #button>
+        <van-button size="small" type="primary" id="btn">忘记密码</van-button>
+      </template>
+    </van-field>
+    <p>注册新用户</p>
+    <!-- 提交按钮 -->
+   <van-button round block type="info" native-type="submit" class="btn">
+      完成提交
+    </van-button>
+  </div>
+  
+</template>
+<script>
+export default {
+  data(){
+
+  },
+  methods: {
+    onClickLeft() {
+      this.$router('/')
+    },
+    onClickLeft() {
+    this.$rooter.push('/register')
+    },
+  },
+}
+// 背景图片的样式
+
+</script>
+<style scope>
+.login{
+  background: url(../assets/img/dl.png);
+  width: 100%;
+ }
+   .login #btn{
+   border-radius: 20px;
+   background-color:#ccc;
+   margin: 0 auto;
+   border: 0;
+   width: 80px;
+  }
+
+</style>
