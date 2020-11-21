af50cbc83ba0e1656b3c0f27c3fef695983aa166
baicy <839975410@qq.com>
2020-11-21 18:20:21 +0800

    登录注册

--- dhz/src/views/Register.vue
+++ dhz/src/views/Register.vue
@@ -91,144 +91,154 @@
   </div>
 </div>
 </template>
+
 <script>
-export default {
-  data(){
-    return{
-      //手机号
-      iserror:false,
-      iserrorMsg:'',
-      text:"",
-      radio: '1',
-      phone:'',
-      value1:'',
-      // 昵称的注释
-      nickname:"",
-      //密码
-      password:'',
-      conpassword:"",
-      d:false
+    export default {
+        data(){
+          return{
+            //手机号
+            iserror:false,
+            iserrorMsg:'',
+            text:"",
+            radio: '1',
+            phone:'',
+            value1:'',
+            // 昵称的注释
+            nickname:"",
+            //密码
+            password:'',
+            conpassword:"",
+            d:false
+       
+          }
+        },
+        methods: {
+          //
+
+          //用户协议校验
+            xy(){
+              if(this.true){
+                this.d=false;
+
+              }else{
+                this.d=true;
+                // return true;
+              }
+              
+            },
+
+          //单击完成提交时得校验
+            handle(){
+              //let radio=this.radio;
+            //   console.log(radio);
+            //   if( this.checkPhone()  && this.checkPassword() ){
+            //      this.$router.push("/")
+            //   }
+              if(this.d){
+                    
+              this.axios.post('/user/register',"&phone="+this.phone +'&password=' + this.password).then( res => {
+                // this.phone = res.data.result;
+                // console.log(res.data.userInfo);
+                this.$router.push("/")
+                });
+
+            } 
+          }, 
+
+          //再次检验验证码
+          checkConpassword() {
+            //校验两次密码是否一致
+            let password = this.password;
+            let conpassword = this.conpassword;
+            if (password == conpassword) {
+              return true;
+            } else {
+              this.$toast({
+                message: "两次密码不一致",
+                position: "middle",
+                duration: 4000,
+              });
+              return false;
+            }
+          },
+
+          //检测密码
+          checkPassword() {
+            let password = this.password;
+            let passwordReg = /^[0-9A-Za-z\.\-_]{6,16}$/;
+            if (passwordReg.test(password)) {
+              this.perror = false;
+              return true;
+            } else {
+              this.$toast({
+                message: "密码错误",
+                position: "middle",
+                duration: 5000,
+              });
+              return false;
+
+            }
+          },
+        
+          // 检测用户手机号
+          checkPhone() {
+            let phone = this.phone;
+            let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+            if (phoneReg.test(phone)) {
+              // 修改手机号的状态
+              this.iserror = false;
+              this.iserrorMsg='';
+              return true;
+            } else {
+              //终止函数的执行
+              this.iserror = true;
+              this.iserrorMsg='输入的手机号错误';
+              return false;
+            }
+          },
+
+          onClickLeft() {
+            this.$router.push('/login')
+          },
+
+        },
+        
+         //注册时的挂载接口
+              mounted() {
+            
+        }
     }
-  },
-  methods: {
-    //用户协议校验
-    xy(){
-      if(this.true){
-        this.d=false;
-
-      }else{
-        this.d=true;
-        // return true;
-      }
-      
-    },
-     //单击完成提交时得校验
-        handle(){
-          //let radio=this.radio;
-        //   console.log(radio);
-        //   if( this.checkPhone()  && this.checkPassword() ){
-        //      this.$router.push("/")
-        //   }
-          if(this.d){
-            this.$router.push("/")
-          } 
-        }, 
     
-
-
-
-    //再次检验验证码
-    checkConpassword() {
-      //校验两次密码是否一致
-      let password = this.password;
-      let conpassword = this.conpassword;
-      if (password == conpassword) {
-        return true;
-      } else {
-        this.$toast({
-          message: "两次密码不一致",
-          position: "middle",
-          duration: 4000,
-        });
-        return false;
-      }
-    },
-
-
-    //检测密码
-    checkPassword() {
-      let password = this.password;
-      let passwordReg = /^[0-9A-Za-z\.\-_]{6,16}$/;
-      if (passwordReg.test(password)) {
-        this.perror = false;
-        return true;
-      } else {
-        this.$toast({
-          message: "密码错误",
-          position: "middle",
-          duration: 5000,
-        });
-        return false;
-
-      }
-    },
-   
-
-    // 检测用户手机号
-    checkPhone() {
-      let phone = this.phone;
-      let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
-      if (phoneReg.test(phone)) {
-        // 修改手机号的状态
-        this.iserror = false;
-        this.iserrorMsg='';
-        return true;
-      } else {
-        //终止函数的执行
-        this.iserror = true;
-        this.iserrorMsg='输入的手机号错误';
-        return false;
-      }
-    },
-
-    onClickLeft() {
-      this.$router.push('/login')
-     },
-   },
-};
 </script>
 
-
 <style>
-/* 底部选择框 */
-/* .register .input{
-  foot-size:
-}; */
-/* 底部样式 */
-.register #xieyi{
-margin-top: 20px;
-margin-left: 20px;
-color:#A7A7A7;
-}
-.register .xy a{
-  color:  #F3AC3F;
-}
 
-/* 按钮样式 */
- .register #button{
-    border-radius: 15px;
-   }
-   .register .btn{
-     border-radius: 5px;
-     width: 90%;
-     background-color: #5ABCC8;
-    margin: 0 auto;
-    border: 0;
-  }
-  /* 性别选择的样式 */
-    .register .van-radio__icon--checked .van-icon{
-    background-color: #F3AC3F;
-    border-color: #F3AC3F;
-  }
+    /* 底部样式 */
+    .register #xieyi{
+    margin-top: 20px;
+    margin-left: 20px;
+    color:#A7A7A7;
+    }
+
+    .register .xy a{
+      color:  #F3AC3F;
+    }
+
+    /* 按钮样式 */
+    .register #button{
+        border-radius: 15px;
+      }
+      .register .btn{
+        border-radius: 5px;
+        width: 90%;
+        background-color: #5ABCC8;
+        margin: 0 auto;
+        border: 0;
+      }
+      /* 性别选择的样式 */
+        .register .van-radio__icon--checked .van-icon{
+        background-color: #F3AC3F;
+        border-color: #F3AC3F;
+      }
 </style>
  
--- server/router/user.js
+++ server/router/user.js
@@ -5,14 +5,30 @@ const pool=require('../pool.js');
 //创建用户路由器
 const u=express.Router();
 
-
-/*添加路由*/
-
-
-
-
-
-
+// 用户注册的接口
+u.post('/register', (req, res) => {
+  //console.log(md5('12345678')) ;
+  //获取用户名和密码
+  let phone = req.body.phone;
+  let password = req.body.password;
+   console.log(phone,password);
+  //查找用户是否存在
+  let sql = 'SELECT COUNT(uid) AS count FROM dhz_users WHERE phone=?';
+  pool.query(sql, [phone],(error, results) => {
+    if (error) throw error;
+    //如果用户不存在,则插入记录
+    if (results[0].count == 0) {
+      sql = 'INSERT INTO dhz_users (phone,password,nickname) VALUES(?,md5(?),?)';
+      pool.query(sql, [phone, password,phone], (error, result) => {
+        if (error) throw error;
+        console.log(result);   
+        res.send({code: 1});
+      })
+    } else { //否则产生合理的错误提示
+      res.send({code: 0 });
+    }
+  })
+});
 
 
 
